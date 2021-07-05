f46fbf258c5fce6cde393f639d2d72049fbf9420
SidheshBokade <86685872+SidheshBokade@users.noreply.github.com>
2021-07-05 20:01:58 +0530

    Updates

---
+++ precache-manifest.eebadd389a56174351ad42d1b4330b48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e523cff663a370fc5bb47873dfc71b0",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "878fe527b9ee5b2b13d7",
+    "url": "/CoronaTracker/static/css/main.138c9f79.chunk.css"
+  },
+  {
+    "revision": "8bc2ae08c081b0ca019a",
+    "url": "/CoronaTracker/static/js/2.5c97dbb6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoronaTracker/static/js/2.5c97dbb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878fe527b9ee5b2b13d7",
+    "url": "/CoronaTracker/static/js/main.1d12496b.chunk.js"
+  },
+  {
+    "revision": "988fb36a40ced2d0eb6f",
+    "url": "/CoronaTracker/static/js/runtime-main.867b7744.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoronaTracker/static/media/image.d7265326.png"
+  }
+]);
